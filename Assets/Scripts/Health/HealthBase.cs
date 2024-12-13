2eff236db803b3180933e1489ca4428f6fd18b05
Vini <vfbrandx6@gmail.com>
2024-12-13 19:59:37 -0300

    added HealthBase
    Adicionado script de sistema de vida capaz de receber dano e matar o gameObject que tiver esse componente

---
+++ Assets/Scripts/Health/HealthBase.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBase : MonoBehaviour
+{
+    #region VARIABLES
+    public int startLife = 10;
+
+    public bool destroyOnKill = false;
+    public float delayToKill = 0f;
+
+    private int _currentLife;
+    private bool _isDead = false;
+
+    #endregion
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    #region FUNCTIONS
+    private void Init()
+    {
+        _isDead = false;
+        _currentLife = startLife;
+    }
+
+    public void Damage(int damage)
+    {
+        if (_isDead) return;
+
+        _currentLife -= damage;
+
+        if (_currentLife < 0)
+        {
+            Kill();
+        }
+
+    }
+
+    private void Kill()
+    {
+        _isDead = true;
+
+        if (destroyOnKill)
+        {
+            Destroy(gameObject, delayToKill);
+        }
+
+
+    }
+        #endregion
+
+}
