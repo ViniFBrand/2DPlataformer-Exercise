cadd49e048d7ec53be351b397c8439088c92323b
Vini <vfbrandx6@gmail.com>
2025-01-18 18:12:54 -0300

    Tried to add coin and items related scripts
    Adicionado moeda com trigger + item manager + item collactable base + item collactable coin
    
    Algum erro acontecente na instancia do Item Manager ao coletar a moeda (não contabilizando)

---
+++ Assets/Scripts/Item/ItemManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ebac.Core.Sigleton;
+
+public class ItemManager : Singleton<ItemManager>
+{
+    public int coins;
+
+    private void Awake()
+    {
+        Reset();
+    }
+
+    private void Reset()
+    {
+        coins = 0;
+    }
+
+    public void AddCoins(int amount = 1)
+    {
+        coins +=amount;
+    }
+}
