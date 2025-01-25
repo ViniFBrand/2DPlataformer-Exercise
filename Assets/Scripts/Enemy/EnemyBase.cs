8e37d2c276b5556a5c4ebddad58fca4bbaa24dad
Vini <vfbrandx6@gmail.com>
2025-01-25 14:41:39 -0300

    Set Enemy Death Animation and added Gun and Projectiles
    Adicionou animação de morte de inimigo ao script
    Adicionou arma e projeteis pra causar a morte ao inimigo

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -5,12 +5,71 @@ using UnityEngine;
 public class EnemyBase : MonoBehaviour
 {
     public int damage = 10;
+    
+    public Animator animator;
+    public string triggerAttack = "Attack";
+    public string triggerIdle = "Idle";
+    public string triggerDeath = "Death";
+
+    public HealthBase healthBase;
+
+    private void Awake()
+    {
+        if(healthBase != null)
+        {
+            healthBase.OnKill += OnEnemyKill;
+        }
+    }
+
+    private void OnEnemyKill()
+    {
+        healthBase.OnKill -= OnEnemyKill;
+        PlayDeathAnimation();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         var health = collision.gameObject.GetComponent<HealthBase>();
 
-        if(health != null )
+        if (health != null)
+        {
             health.Damage(damage);
+        }
     }
+
+    /* private void OnCollisionExit2D(Collision2D collision)
+     {
+         PlayIdleAnimation();
+     }*/
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayAttackAnimation();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        PlayIdleAnimation();
+    }
+
+    private void PlayAttackAnimation()
+    {
+        animator.SetTrigger(triggerAttack);
+    }
+
+    private void PlayIdleAnimation()
+    {
+        animator.SetTrigger(triggerIdle);
+    }
+
+    private void PlayDeathAnimation()
+    {
+        animator.SetTrigger(triggerDeath);
+    }
+
+    public void Damage(int amount)
+    {
+        healthBase.Damage(amount);
+    }
+
 }
